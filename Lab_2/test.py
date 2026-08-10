00d3bf8b87d744ffc0648a2118b77395dcc6346c
jenil-149 <jenil140509@gmail.com>
2026-08-10 19:42:58 +0530

    Add Lab 2 environment setup

---
+++ Lab_2/test.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    google_api_key=api_key
+)
+
+result = model.invoke("What is the full form of DAIICT?")
+
+print(result.content)
